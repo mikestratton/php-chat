b84b08cc7a405e5fd5447a940e6745602bcfe9c5
mikestratton <mike@mikestratton.net>
2017-11-22 02:10:14 -0500

    iphone safari displaying apostrophe's as diamond with question mark as a result of a problem with safari's default font. changed font to open sans, problem resolved

--- index.php
+++ index.php
@@ -20,6 +20,7 @@ function random_color() {
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
+<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
 
 <link rel="stylesheet" href="style.css" media="all" />
 <script>
